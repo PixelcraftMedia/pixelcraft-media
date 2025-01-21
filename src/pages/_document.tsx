3786f1a2981f8130b4cd409edaa560d0df4295bf
VITALIY1986 <staslego1986@gmail.com>
2025-01-21 19:41:44 +0200

    asdsdgg

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
 	return (
 		<Html >
-			<Head />
+			 <Head>
+			 <meta name="google-site-verification" content="FMwG9ZYEJRdIBWSfuZrvNDNP-zY4Z56YlKPHkpx-M30" />
+      </Head>
 			<body>
 			
 				<Main />
